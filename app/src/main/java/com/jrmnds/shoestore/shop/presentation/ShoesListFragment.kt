f6ede47f9b9b652e10d1e52d5fa691caab6cc423
Reginaldo Mendes Junior <rmendesj@SAO-C02TN40JHV2L.local>
2022-07-07 22:26:26 -0300

    Creation of shoe_list_card

--- app/src/main/java/com/jrmnds/shoestore/shop/presentation/ShoesListFragment.kt
+++ app/src/main/java/com/jrmnds/shoestore/shop/presentation/ShoesListFragment.kt
@@ -5,13 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.jrmnds.shoestore.R
+import com.jrmnds.shoestore.databinding.FragmentShoesListBinding
 
 class ShoesListFragment : Fragment() {
+
+    private lateinit var shoesListBinding: FragmentShoesListBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_shoes_list, container, false)
+    ): View {
+        setBinders(inflater, container)
+        return shoesListBinding.root
+    }
+
+    private fun setBinders(inflater: LayoutInflater, container: ViewGroup?) {
+        shoesListBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_shoes_list,
+            container,
+            false
+        )
     }
 }
