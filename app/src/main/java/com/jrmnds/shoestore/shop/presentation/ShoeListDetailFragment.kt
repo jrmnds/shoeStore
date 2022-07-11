2b326164ad73d2585a0c2eb92b5c83cbeea1c44f
Reginaldo Mendes Junior <rmendesj@SAO-C02TN40JHV2L.local>
2022-07-11 20:26:54 -0300

    Changes on shoeListDetailFragment and observers for shoeListdata

--- app/src/main/java/com/jrmnds/shoestore/shop/presentation/ShoeListDetailFragment.kt
+++ app/src/main/java/com/jrmnds/shoestore/shop/presentation/ShoeListDetailFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import com.jrmnds.shoestore.R
 import com.jrmnds.shoestore.databinding.FragmentShoeListDetailBinding
 
@@ -14,12 +15,14 @@ import com.jrmnds.shoestore.databinding.FragmentShoeListDetailBinding
 class ShoeListDetailFragment : Fragment() {
 
     private lateinit var bindingShoeListDetail: FragmentShoeListDetailBinding
+    private val shoesListViewModel: ShoesListViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         setBinders(inflater, container)
+        observeFields()
         return bindingShoeListDetail.root
     }
 
@@ -32,6 +35,46 @@ class ShoeListDetailFragment : Fragment() {
         )
 
         bindingShoeListDetail.lifecycleOwner = this
+        bindingShoeListDetail.shoeDetailsViewModel = shoesListViewModel
         (activity as AppCompatActivity).supportActionBar?.title = "Add a new Shoe"
+    }
+
+   private fun observeFields(){
+        shoesListViewModel.notNullNameShoe.observe(viewLifecycleOwner){ shoeName ->
+            when {
+                shoeName.isEmpty() -> {
+                    bindingShoeListDetail.shoeNameLabelId.error = getString(R.string.shoe_name_not_empty)
+                    bindingShoeListDetail.shoeNameLabelId.requestFocus()
+                }
+            }
+        }
+
+       shoesListViewModel.notNullshoeCompany.observe(viewLifecycleOwner){ shoeCompany ->
+           when{
+               shoeCompany.isEmpty() -> {
+                   bindingShoeListDetail.shoesCompanyLabelId.error = getString(R.string.shoes_company_not_empty)
+                   bindingShoeListDetail.shoesCompanyLabelId.requestFocus()
+               }
+           }
+       }
+
+       shoesListViewModel.notNullshoeSize.observe(viewLifecycleOwner){ shoeSize ->
+           when{
+               shoeSize.isEmpty() -> {
+                   bindingShoeListDetail.sizeLabelId.error = getString(R.string.shoe_size_not_empty)
+                   bindingShoeListDetail.sizeLabelId.requestFocus()
+               }
+           }
+       }
+
+       shoesListViewModel.notNullshoeDescription.observe(viewLifecycleOwner){ shoeDescription ->
+           when{
+               shoeDescription.isEmpty() ->{
+                   bindingShoeListDetail.descriptionLabelId.error = getString(R.string.shoe_description_not_empty)
+                   bindingShoeListDetail.descriptionLabelId.requestFocus()
+               }
+           }
+       }
+
     }
 }
--- app/src/main/java/com/jrmnds/shoestore/shop/presentation/ShoesListViewModel.kt
+++ app/src/main/java/com/jrmnds/shoestore/shop/presentation/ShoesListViewModel.kt
@@ -12,13 +12,47 @@ class ShoesListViewModel: ViewModel() {
     private lateinit var shoe: Shoe
     private lateinit var shoesListData: ShoesList
 
+    var imageShoe: String = ""
+    var shoeName: String = ""
+    var shoeCompany: String = ""
+    var shoeDescription: String = ""
+    var shoeSize: String = ""
+
     private var _shoeList = MutableLiveData<ShoesList>()
+    private var _notNullNameShoe = MutableLiveData<String>()
+    private var _notNullshoeCompany = MutableLiveData<String>()
+    private var _notNullshoeDescription = MutableLiveData<String>()
+    private var _notNullshoeSize = MutableLiveData<String>()
+    private var _notNullShoeImage = MutableLiveData<String>()
+
+    private val _shouldGoToTheNextPage = MutableLiveData<Boolean>()
+
 
     val shoeList: LiveData<ShoesList>
         get() = _shoeList
 
+    val notNullNameShoe: LiveData<String>
+        get() = _notNullNameShoe
+
+    val notNullshoeCompany: LiveData<String>
+        get() = _notNullshoeCompany
+
+    val notNullshoeDescription: LiveData<String>
+        get() = _notNullshoeDescription
+
+    val notNullshoeSize: LiveData<String>
+        get() = _notNullshoeSize
+
+    val notNullShoeImage: LiveData<String>
+        get() = _notNullShoeImage
+
+    val shouldGoToTheNextPage: LiveData<Boolean>
+        get() = _shouldGoToTheNextPage
+
+
     init {
         createShoeObjectsToList()
+        imageShoe = Base64ExampleImage.base64TestImage
     }
 
     private fun createShoeObjectsToList(){
@@ -33,6 +67,38 @@ class ShoesListViewModel: ViewModel() {
         shoesListData.shoesList.add(shoe)
         _shoeList.value = shoesListData
     }
+
+    fun setShoeData(){
+        _notNullNameShoe.value = shoeName
+        _notNullshoeCompany.value = shoeCompany
+        _notNullshoeSize.value = shoeSize
+        _notNullshoeDescription.value = shoeDescription
+        _notNullShoeImage.value = imageShoe
+        addShoeToShoeList(createShoeObject())
+        validateShoesField()
+    }
+
+    private fun addShoeToShoeList(shoe: Shoe) {
+        shoesListData.shoesList.add(shoe)
+        _shoeList.value = shoesListData
+    }
+
+    private fun createShoeObject() : Shoe {
+      return shoe.apply {
+            name = notNullNameShoe.value.toString()
+            company = notNullshoeCompany.value.toString()
+            description = notNullshoeDescription.value.toString()
+            size = notNullshoeSize.value!!.toDouble()
+            image = imageShoe
+        }
+    }
+
+    private fun validateShoesField(){
+        _shouldGoToTheNextPage.value = (_notNullNameShoe.value!!.isNotEmpty() && _notNullshoeCompany.value!!.isNotEmpty()
+                && _notNullshoeSize.value!!.isNotEmpty() && _notNullshoeDescription.value!!.isNotEmpty() &&
+                _notNullShoeImage.value!!.isNotEmpty())
+    }
+
 
 
 }
